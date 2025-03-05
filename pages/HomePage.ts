cd8ec1ee72177ebab314f5d7b89b58b108752f2b
wesoolowsky <michal.wesolowski97@gmail.com>
2025-03-05 12:43:41 +0100

    fix: fix for the smaller view

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -7,6 +7,7 @@ export class HomePage {
   readonly oneTrustAccept: Locator;
   readonly ageConfirmation: Locator;
   readonly shopLink: Locator;
+  readonly hamburgerMenu: Locator;
 
   constructor(page: Page, market: Market = 'uk') {
     this.page = page;
@@ -14,14 +15,21 @@ export class HomePage {
     this.oneTrustAccept = page.locator('#onetrust-accept-btn-handler');
     this.ageConfirmation = page.locator('.ageconfirmation__confirmBtn');
     this.shopLink = page.locator('.navigation__linkWrapper').getByText(markets[market].shopLink);
+    this.hamburgerMenu = page.locator('.navigation__hamburger');
   }
 
   async goto() {
     await this.page.goto(`${markets[this.market].baseUrl}`);
     await this.oneTrustAccept.click();
     await this.ageConfirmation.click();
-    await this.shopLink.click();
-    await this.page.locator('.header__wrapper').hover();
+    const isHamburgerVisible = await this.hamburgerMenu.isVisible();
+    if (isHamburgerVisible) {
+      await this.hamburgerMenu.click();
+      await this.shopLink.click();
+    }
+    else{
+      await this.shopLink.click();
+    }
   }
 
   async goToShopPage(): Promise<void> {
